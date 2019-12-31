2c8fce2f0c97e90296dbcc36899a54df9aef9717
Tomasz Sawka <sawka.tomek@gmail.com>
2019-12-31 13:17:02 +0100

    IntCodePC add input and output instructions.

--- src/day2.rs
+++ src/day2.rs
@@ -8,10 +8,10 @@ pub struct Day2 {
 
 impl Day2 {
 
-    pub fn read_program(&self) -> Vec<usize> {
+    pub fn read_program(&self) -> Vec<i32> {
         let path = self.input();
         let lines = self.read_input_lines_string(&path);
-        let program = lines[0].split(',').map(|t| t.parse::<usize>().unwrap()).collect();
+        let program = lines[0].split(',').map(|t| t.parse::<i32>().unwrap()).collect();
         program
     }
 }
@@ -19,7 +19,9 @@ impl Day2 {
 impl Day for Day2 {
     fn first_puzzle(&self) -> String {
         let program = self.read_program();
-        let mut pc = IntCodePC::new(program);
+        let mut i = std::io::empty();
+        let mut o = std::io::sink();
+        let mut pc = IntCodePC::new(program, &mut i, &mut o);
         pc.alert1202();
         let ret = pc.run();
         format!("{}", ret)
@@ -28,7 +30,10 @@ impl Day for Day2 {
     fn second_puzzle(&self) -> String {
 
         let program = self.read_program();
-        let mut pc = IntCodePC::new(program.clone());
+
+        let mut i = std::io::empty();
+        let mut o = std::io::sink();
+        let mut pc = IntCodePC::new(program.clone(), &mut i, &mut o);
 
         for noun in 0..99 {
             for verb in 0 .. 99 {
--- src/intcode.rs
+++ src/intcode.rs
@@ -1,35 +1,42 @@
+use std::error::Error;
+use std::io::prelude::*;
 
 #[derive(PartialEq, Debug)]
 pub enum Opcode {
     Add,
     Mul,
-    Halt(usize)
+    Input,
+    Output,
+    Halt(i32)
 }
 
-pub struct IntCodePC {
-    program: Vec<usize>,
-    pc: usize
+pub struct IntCodePC<'i, 'o> {
+    program: Vec<i32>,
+    pc: usize,
+    i: &'i mut dyn BufRead,
+    o: &'o mut dyn Write
 }
 
-impl IntCodePC {
-    pub fn new(program: Vec<usize>) -> IntCodePC {
-        IntCodePC {program, pc: 0}
+impl<'i, 'o> IntCodePC<'i, 'o> {
+
+    pub fn new<I: BufRead, O: Write>(program: Vec<i32>, i: &'i mut I, o: &'o mut O) -> IntCodePC<'i, 'o> {
+        IntCodePC {program, pc:0, i, o}
     }
 
-    pub fn halt(&self) -> usize {
+    pub fn halt(&self) -> i32 {
         self.program[0]
     }
 
-    pub fn read(&mut self) -> usize {
+    pub fn read(&mut self) -> i32 {
         let pos = self.program[self.pc + 1];
         self.pc += 1;
-        self.program[pos]
+        self.program[pos as usize]
     }
 
-    pub fn write(&mut self, val: usize) {
+    pub fn write(&mut self, val: i32) {
         let pos = self.program[self.pc + 1];
         self.pc += 1;
-        self.program[pos] = val;
+        self.program[pos as usize] = val;
     }
 
     pub fn add(&mut self) {
@@ -44,17 +51,42 @@ impl IntCodePC {
         self.pc += 1; 
     }
 
+    pub fn input(&mut self) {
+        let pos = self.read();
+        let mut buf = String::new();
+        match self.i.read_line(&mut buf) {
+            Ok(_) => {
+                match buf.trim().parse::<i32>() {
+                    Ok(val) => {
+                        self.program[pos as usize] = val;
+                    },
+                    Err(e) => {
+                        panic!(format!("NaN: {}", e.description()))}
+                }
+            },
+            Err(e) => panic!(format!("Failed to read from cin: {}", e.description()))
+        }
+    }
+
+    pub fn output(&mut self) {
+       let val = self.read(); 
+       match write!(self.o, "{}", val) {
+          Ok(_) => {},
+          Err(e) => panic!(format!("Failed to write to stdout: {}", e.description()))
+       }
+    }
+
     pub fn alert1202(&mut self) {
         self.program[1] = 12;
         self.program[2] = 02;
     }
 
-    pub fn reset(&mut self, program: Vec<usize>) {
+    pub fn reset(&mut self, program: Vec<i32>) {
         self.program = program;
         self.pc = 0;
     }
 
-    pub fn init(&mut self, noun: usize, verb: usize) {
+    pub fn init(&mut self, noun: i32, verb: i32) {
         self.program[1] = noun;
         self.program[2] = verb;
     }
@@ -70,6 +102,14 @@ impl IntCodePC {
                 self.mul();
                 Opcode::Mul
             },
+            3 => {
+                self.input();
+                Opcode::Input
+            },
+            4 => {
+                self.output();
+                Opcode::Output
+            },
             99 => {
                 Opcode::Halt(self.halt())
             },
@@ -79,7 +119,7 @@ impl IntCodePC {
         }
     }
 
-    pub fn run(&mut self) -> usize {
+    pub fn run(&mut self) -> i32 {
         loop {
             match self.step() {
                 Opcode::Halt(val) => return val,
@@ -97,7 +137,9 @@ mod tests
 
     #[test]
     fn add() {
-        let mut pc = IntCodePC::new(vec![1,9,10,3,2,3,11,0,99,30,40,50]);
+        let mut o = std::io::sink();
+        let mut i = std::io::empty();
+        let mut pc = IntCodePC::new(vec![1,9,10,3,2,3,11,0,99,30,40,50], &mut i, &mut o);
 
         assert_eq!(pc.step(), Opcode::Add);
         assert_eq!(pc.program, vec![1,9,10,70,2,3,11,0,99,30,40,50]);
@@ -107,7 +149,10 @@ mod tests
 
     #[test]
     fn mul() {
-        let mut pc = IntCodePC::new(vec![1,9,10,70,2,3,11,0,99,30,40,50]);
+
+        let mut o = std::io::sink();
+        let mut i = std::io::empty();
+        let mut pc = IntCodePC::new(vec![1,9,10,70,2,3,11,0,99,30,40,50], &mut i, &mut o);
         pc.pc = 4;
 
         assert_eq!(pc.step(), Opcode::Mul);
@@ -116,8 +161,33 @@ mod tests
     }
 
     #[test]
+    fn input() {
+
+        let buf = b"15";
+        let mut i = &buf[..];
+        let mut o = std::io::sink();
+        let mut pc = IntCodePC::new(vec![3,2,3, 0], &mut i, &mut o);
+        assert_eq!(pc.step(), Opcode::Input);
+        assert_eq!(vec![3, 2, 3, 15], pc.program);
+    }
+
+    #[test]
+    fn output() {
+
+        let mut i = std::io::empty();
+        let mut o: Vec<u8> = Vec::new();
+        let mut pc = IntCodePC::new(vec![4,2,15], &mut i, &mut o);
+        assert_eq!(pc.step(), Opcode::Output);
+        let s = String::from_utf8(o).unwrap();
+        assert_eq!(s, "15");
+    }
+
+    #[test]
     fn halt() {
-        let mut pc = IntCodePC::new(vec![3500,9,10,70,2,3,11,0,99,30,40,50]);
+
+        let mut o = std::io::sink();
+        let mut i = std::io::empty();
+        let mut pc = IntCodePC::new(vec![3500,9,10,70,2,3,11,0,99,30,40,50], &mut i, &mut o);
         pc.pc = 8;
 
         assert_eq!(pc.step(), Opcode::Halt(3500));
@@ -125,20 +195,23 @@ mod tests
 
     #[test]
     fn run() {
-        let mut pc = IntCodePC::new(vec![1,9,10,3,2,3,11,0,99,30,40,50]);
+
+        let mut i = std::io::empty();
+        let mut o = std::io::sink();
+
+        let mut pc = IntCodePC::new(vec![1,9,10,3,2,3,11,0,99,30,40,50], &mut i, &mut o);
         assert_eq!(pc.run(), 3500);
 
-        let mut pc = IntCodePC::new(vec![1,0,0,0,99]);
+        let mut pc = IntCodePC::new(vec![1,0,0,0,99], &mut i, &mut o);
         assert_eq!(pc.run(), 2);
 
-        let mut pc = IntCodePC::new(vec![2,3,0,3,99]);
+        let mut pc = IntCodePC::new(vec![2,3,0,3,99], &mut i, &mut o);
         assert_eq!(pc.run(), 2);
 
-        let mut pc = IntCodePC::new(vec![2,4,4,5,99,0]);
+        let mut pc = IntCodePC::new(vec![2,4,4,5,99,0], &mut i, &mut o);
         assert_eq!(pc.run(), 2);
 
-        let mut pc = IntCodePC::new(vec![1,1,1,4,99,5,6,0,99]);
+        let mut pc = IntCodePC::new(vec![1,1,1,4,99,5,6,0,99], &mut i, &mut o);
         assert_eq!(pc.run(), 30);
     }
-
 }
